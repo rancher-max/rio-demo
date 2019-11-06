76888136548cfac3c98df25ae9c63ab432f9caeb
rancher-max <55997940+rancher-max@users.noreply.github.com>
2019-11-06 11:45:32 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Rio:PR from HOOK")
+	fmt.Fprintln(w, "Hi there, I'm running in Rio:manual change master 1")
 }
 
 func main() {
