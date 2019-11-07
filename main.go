7d709a861ae0c83a073853ee20b04117b356d1f0
rancher-max <55997940+rancher-max@users.noreply.github.com>
2019-11-07 11:03:41 -0700

    Sleep for 2 seconds

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
-	time.Sleep(101 * time.Millisecond)
+	time.Sleep(2001 * time.Millisecond)
 	fmt.Fprintln(w, "Hi there, I'm running in Rio:hope:v2")
 }
 
