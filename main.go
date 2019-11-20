3bd17c30708d1f340def6ede91487c9cda71fba1
rancher-max <rancher-max@users.noreply.github.com>
2019-11-20 09:36:26 -0700

    Update main.go

--- main.go
+++ main.go
@@ -2,42 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"time"
 )
 
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received request to FOO")
-	fmt.Fprintln(w, "Hi there, I'm running in Rio:FOO port 8001")
-}
-
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	// Arbitrary sleep so that we can demonstrate autoscaler
-	log.Println("Received request to BAR")
-	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Rio:BAR port 8002")
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	// Arbitrary sleep so that we can demonstrate autoscaler
-	log.Println("Basic logging -- request received")
-	time.Sleep(501 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Rio:handler func on port 8080")
-}
-
 func main() {
+	finish := make(chan bool)
+
+	server8001 := http.NewServeMux()
+	server8001.HandleFunc("/foo", foo8001)
+	server8001.HandleFunc("/bar", bar8001)
+
+	server8002 := http.NewServeMux()
+	server8002.HandleFunc("/foo", foo8002)
+	server8002.HandleFunc("/bar", bar8002)
+
 	go func() {
-		http.HandleFunc("/foo", fooHandler)
-		log.Fatal(http.ListenAndServe(":8001", nil))
+		http.ListenAndServe(":8001", server8001)
 	}()
 
 	go func() {
-		http.HandleFunc("/", handler)
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		http.ListenAndServe(":8002", server8002)
 	}()
 
-	//the last call is outside goroutine to avoid that program just exit
-	http.HandleFunc("/bar", barHandler)
-	log.Fatal(http.ListenAndServe(":8002", nil))
+	<-finish
+}
+
+func foo8001(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Listening on 8001: foo ")
+}
+
+func bar8001(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Listening on 8001: bar ")
+}
+
+func foo8002(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Listening on 8002: foo ")
+}
+
+func bar8002(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Listening on 8002: bar ")
 }
