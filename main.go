aa91efe60ee5d283d381970ad9f50b6167df0a50
rancher-max <rancher-max@users.noreply.github.com>
2019-11-20 10:09:09 -0700

    Add default handler

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ func main() {
 	finish := make(chan bool)
 
 	server8001 := http.NewServeMux()
+	server8001.HandleFunc("/", default8001)
 	server8001.HandleFunc("/foo", foo8001)
 	server8001.HandleFunc("/bar", bar8001)
 
 	server8002 := http.NewServeMux()
+	server8002.HandleFunc("/", default8002)
 	server8002.HandleFunc("/foo", foo8002)
 	server8002.HandleFunc("/bar", bar8002)
 
@@ -27,12 +29,20 @@ func main() {
 	<-finish
 }
 
+func default8001(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Listening on 8001")
+}
+
 func foo8001(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Listening on 8001: foo ")
 }
 
 func bar8001(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Listening on 8001: bar ")
+}
+
+func default8002(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Listening on 8002")
 }
 
 func foo8002(w http.ResponseWriter, r *http.Request) {
