3909df3542c78f4fc42371ceebb5f2a1bfdb5919
Max <55997940+rancher-max@users.noreply.github.com>
2019-12-16 16:14:40 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Max rio demo v2")
+	fmt.Fprintln(w, "Max rio demo testing branch")
 }
 
 func main() {
