33e9374e058043bd169b7cc2f531d9a0e7889b25
Max <55997940+rancher-max@users.noreply.github.com>
2019-12-16 16:32:44 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Testing tag creation with buildpr")
+	fmt.Fprintln(w, "1 2 3 4 I declare a thumb war")
 }
 
 func main() {
