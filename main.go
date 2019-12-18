45ee8af822490e37e9e0d7c7461227f7319c8d73
Max <55997940+rancher-max@users.noreply.github.com>
2019-12-18 12:01:30 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Here we go")
+	fmt.Fprintln(w, "I should never see this")
 }
 
 func main() {
