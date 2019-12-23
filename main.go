38a3bc8876af2502412b92b93544d72f8d7d27f9
Max <55997940+rancher-max@users.noreply.github.com>
2019-12-23 12:47:57 -0700

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	log.Println("Basic logging -- request received")
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Tag PR")
+	fmt.Fprintln(w, "Direct commit to master")
 }
 
 func main() {
